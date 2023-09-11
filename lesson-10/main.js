0afdfb1e2bbd062726e693d538f7bed933aaaf8e
katas8n <kirka6061512@icloud.com>
2023-09-11 22:08:49 +0200

    Added homework

--- lesson-10/main.js
+++ lesson-10/main.js
@@ -1,12 +1,100 @@
 import "./style.css";
 
-// const registeredList = []
+// HW :
+// 1) Create profile page
+// Profile page contains following parts :
+// - User name
+// - Photo (by default there is an image which represents question mark)
+// - Date and time , when user was registered .
+// *** 2) User might change an info about himself/herself
 
-// User schema => { login : ,password : , hasRemembered: }
+// Toggler definition
+let hasAccount = false;
+let authorised = false;
 
-// If user has already registered and was logedIn -> he should look at "Log out" so "Register" button must be hidden
-// If user hasn't registered yet -> he should look at "Register"
+// DB
+const accounts = [];
 
-// If user clicked on "Login" btn => He should get two fields -> login / password
+// DOM . Recieve documents nodes
+const userName = document.querySelector("[name='nickname']");
+const userPassword = document.querySelector("[name='password']");
+const sendBtn = document.querySelector("#send-btn");
 
-// Only users which have already have an account can see "Profile"
+const btns = document.querySelector(".nav").children;
+
+btnsReview(hasAccount, accounts, btns);
+
+// Research
+const loginBtns = [...btns].filter(
+  (btn) => toLowerCase(btn.textContent) === "login"
+);
+const loginBtn = getFirst(loginBtns);
+
+// Handlers
+
+sendBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const nickname = userName.value;
+  const password = userPassword.value;
+
+  accounts.push({ nickname, password });
+  clearInputs(userName, userPassword);
+
+  hasAccount = true;
+  btnsReview(hasAccount, accounts, btns, authorised);
+});
+
+loginBtn.addEventListener("click", (e) => {
+  const account = getAccount(accounts, { userName, userPassword });
+
+  if (!account) return console.log("There is not valid data");
+
+  authorised = true;
+
+  btnsReview(hasAccount, accounts, btns, authorised);
+});
+
+function btnsReview(hasAccount, accounts, btns, authorised) {
+  if (!hasAccount || accounts.length === 0) return;
+
+  for (const btn of btns) {
+    const btnsText = btn.textContent;
+    if (!authorised && toLowerCase(btnsText) === "profile") continue;
+
+    btn.removeAttribute("disabled");
+  }
+}
+
+function getAccount(accounts, { userName, userPassword }) {
+  return accounts.filter(
+    (account) =>
+      account.nickname === userName.value &&
+      account.password === userPassword.value
+  )[0];
+}
+
+// utils functions
+
+function toLowerCase(str) {
+  return str.toLowerCase();
+}
+
+function getFirst(arr) {
+  return arr[0];
+}
+
+function clearInputs(...inputs) {
+  for (const input of inputs) {
+    input.value = "";
+  }
+}
+
+//                  yyyy-mm-dd
+// const date = new Date(2023, 11, 1); // 2023.11.01
+// const date = new Date(); // Current : year , month , day + time + GMT format
+// const date = new Date("2023-11-01"); // Current : year , month , day + time + GMT format
+// const date = new Date("Novermber 13 , 2014 "); // Current : year , month , day + time + GMT format
+
+// ! to show date on the screen u should lead it to string
+// console.log("[date]", date);
