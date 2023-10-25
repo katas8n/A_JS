9ab2dbd52d6af2bb0cbe0028dc1afa07477becf9
katas8n <kirka6061512@icloud.com>
2023-10-25 21:28:12 +0200

    HomeWork-18

--- lesson-18/main.js
+++ lesson-18/main.js
@@ -1,24 +1,81 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+// Lead every promise to async/await syntax
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+import {
+  App,
+  Header,
+  Main,
+  Footer,
+  Form,
+  Button,
+  Input,
+  Spinner,
+} from "./components";
+import { Component } from "./core";
 
-setupCounter(document.querySelector('#counter'))
+import "./public/style.scss";
+
+const header = new Header({}).toHTML();
+const main = new Main({}).toHTML();
+const footer = new Footer({}).toHTML();
+
+const app = new App({
+  children: [header, main, footer],
+}).toHTML();
+
+const portal = new Component({
+  tagName: "div",
+  className: "portal--active",
+}).toHTML();
+
+const spiner = new Spinner({}).toHTML();
+
+portal.append(spiner);
+
+document.body.append(portal, app);
+
+fetch("https://swapi.dev/api/")
+  .then((res) => {
+    const json = res.json();
+    return json;
+  })
+  .then((res) => {
+    // decrease nested lvls
+    for (const key in res) {
+      header.append(
+        new Button({
+          textContent: key,
+          events: {
+            click: () => {
+              fetch(`https://swapi.dev/api/${key}`)
+                .then((res) => {
+                  return res.json();
+                })
+                .then((res) => {
+                  console.log("[res]", res);
+                });
+            },
+          },
+        }).toHTML()
+      );
+    }
+  });
+
+// Write function which will recieve data
+// Write function which will append data on the screen
+fetch("https://swapi.dev/api/films/")
+  .then((res) => {
+    const json = res.json();
+    return json;
+  })
+  .then((res) => {
+    portal.className = "portal";
+
+    res.results.forEach((el) => {
+      main.innerHTML += `
+            <h3>${el.title}</h3>
+        `;
+    });
+  })
+  .catch((e) => {
+    console.log("[e]", e);
+  });
